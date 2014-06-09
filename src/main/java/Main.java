4eac959f59e9b46751d6d477cf97e113ff6ead79
tomas <tomasabril@users.noreply.github.com>
2014-06-09 10:44:03 -0300

    show only my name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,7 @@
  * IF62C - Fundamentos de Programação 2
  * 
  * Template de projeto de programa Java usando Maven.
- * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
- * modificado por Tomás Abril
+ * por Tomás Abril
  */
 public class Main
 {
